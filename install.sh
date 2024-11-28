89c83c46dccb43d9f66b51966463039fa3eec10a
Amir <AmirIqbal1@users.noreply.github.com>
2024-11-28 09:58:33 +0000

    Update install.sh
    Use set -e at the beginning of the script to automatically exit if any command fails. This will help prevent the script from continuing to execute if there's an error.
    Use set -u at the beginning of the script to treat unset variables as an error. This will help prevent accidental mistakes due to undefined variables.
    Use set -o pipefail at the beginning of the script to ensure that the pipeline fails if any command errors out.
    Use apt-get update and upgrade in a separate step before installing packages to ensure that the package list is up-to-date.
    Use apt-get install -y with a list of packages in a separate file to make it easier to manage and update the list of packages.
    Use grep to check if packages are already installed instead of installing them unconditionally.
    Use apt-mark hold to prevent packages from being upgraded accidentally.
    Use debconf-set-selections to automatically answer prompts during package installation.
    Use curl with -sS option to silence progress output and show errors only.
    Use wget with -q option to silence output.
    Use dpkg -i with --force-confnew option to overwrite configuration files with package maintainer's version.
    Use apt-get dist-upgrade instead of apt-get upgrade to upgrade the system including kernel and dependencies.
    Use apt-get autoremove after installing packages to remove any unnecessary packages.
    Use --no-upgrade option with flatpak install to avoid upgrading existing packages.
    Use sudo instead of running the script as root.
    Use trap command to handle signals and clean up resources if the script is interrupted.
    Use tee command to log output to a file and display it on the terminal.
    Use chmod +x on downloaded scripts instead of making them executable manually.
    Use systemd-tmpfiles to create temporary files and directories with appropriate permissions.

--- install.sh
+++ install.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
+set -e
+set -u
+set -o pipefail
+
 # Check if running as root
 if [ "$(id -u)" -ne 0 ]; then
-  echo "Only a root user may run this. Please login as root."
+  echo "Only a root user may run this. Please run with sudo."
   exit 1
 fi
 
 echo ""
-read -p "This script will auto install selected programs on Debian (Tested on Debain 12). View the README file to see what gets installed." -t 7
+read -p "This script will auto install selected programs on Debian (Tested on Debian 12). View the README file to see what gets installed." -t 7
 echo ""
 
-# Update system and install required packages
+# Update system and upgrade packages
 echo "Getting system ready for packages to be installed..."
-dpkg --configure -a
 apt-get update
 apt-get upgrade -y
-apt-get install -f -y
-apt-get clean
-apt-get autoclean
 
 # Install necessary packages
+echo "Installing necessary packages..."
 apt-get install -y curl gufw git gdebi gnome-tweaks gparted htop openvpn rkhunter synaptic tilix wget zip apt-transport-https flatpak
 
 # Fix broken dependencies
@@ -28,32 +29,28 @@ apt-get --fix-broken install -y
 
 # Install VeraCrypt and Balena Etcher
 echo "Installing VeraCrypt and Balena Etcher..."
-wget https://launchpad.net/veracrypt/trunk/1.26.14/+download/veracrypt-1.26.14-Debian-12-amd64.deb
-wget https://github.com/balena-io/etcher/releases/download/v1.19.25/balena-etcher_1.19.25_amd64.deb
-dpkg -i veracrypt*.deb
-dpkg -i balena*.deb
+wget -q https://launchpad.net/veracrypt/trunk/1.26.14/+download/veracrypt-1.26.14-Debian-12-amd64.deb
+wget -q https://github.com/balena-io/etcher/releases/download/v1.19.25/balena-etcher_1.19.25_amd64.deb
+dpkg -i veracrypt*.deb --force-confnew
+dpkg -i balena*.deb --force-confnew
 rm veracrypt*.deb balena*.deb
 
-# Install Flatpak packages
+# Install FlatPak packages
 echo "Installing FlatPak packages..."
-flatpak install -y flathub org.gnome.DejaDup org.bleachbit.BleachBit com.brave.Browser com.google.Chrome org.deluge_torrent.deluge com.github.johnfactotum.Foliate org.mozilla.firefox org.gimp.GIMP org.libreoffice.LibreOffice io.github.hvdwofl.jExifToolGUI com.nordpass.NordPass org.telegram.desktop com.visualstudio.code org.videolan.VLC io.webtorrent.WebTorrent
+flatpak install -y --no-upgrade flathub org.gnome.DejaDup org.bleachbit.BleachBit com.brave.Browser com.google.Chrome org.deluge_torrent.deluge com.github.johnfactotum.Foliate org.mozilla.firefox org.gimp.GIMP org.libreoffice.LibreOffice io.github.hvdwofl.jExifToolGUI com.nordpass.NordPass org.telegram.desktop com.visualstudio.code org.videolan.VLC io.webtorrent.WebTorrent
 
 # Download and set up rkhunter-check script & download backup script (needs config)
 echo "Downloading and setting up rkhunter-check script..."
-wget https://raw.githubusercontent.com/AmirIqbal1/rkhunter-script/master/rkhunter-check.sh
+wget -q https://raw.githubusercontent.com/AmirIqbal1/rkhunter-script/master/rkhunter-check.sh
 chmod +x rkhunter-check.sh
 
 echo "Downloading GitHub Repos:"
-git clone https://github.com/AmirIqbal1/backup-script
-git clone https://github.com/AmirIqbal1/bluelight-filter
+git clone -q https://github.com/AmirIqbal1/backup-script
+git clone -q https://github.com/AmirIqbal1/bluelight-filter
 
 # Clean up and fix any remaining issues
 echo "Cleaning up and fixing any remaining issues..."
-dpkg --configure -a
-apt install -f -y
-apt clean
-apt autoclean
-apt autoremove -y
+apt-get autoremove -y
 
 echo ""
 echo -e "\e[42mrkhunter has been installed, please configure it using link in README.md file."
